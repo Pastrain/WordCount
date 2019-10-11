d5aa9be5e5c5c434867cf5c7ef4713282098ff82
xiaxiaoshux <2597759547@qq.com>
2019-10-11 17:16:59 +0800

    1th version

--- 201731062213/PairProgramming/PairProgramming/common.h
+++ 201731062213/PairProgramming/PairProgramming/common.h
@@ -15,4 +15,7 @@ bool isSplit(char chSplit);
 map<string, int> frequency(string **str, int& count);
 void countLine(ifstream &ifile, int *cntLines);
 void countChar(ifstream &ifile, int *cntChar);
-map<string, int> countWords(ifstream &ifile, int &idx)
+string** countWords(ifstream &ifile, map<string, int> &freq, int &idx, int &idy, int &allWords);
+
+bool cmp_by_value(const PAIR& lhs, const PAIR& rhs);
+void mostWords(vector<string, int> &vec, int num);
